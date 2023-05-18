c8ee3c8702c989d7434801ffe8bafc1f42f4751f
Hung Pham <truonghungit@gmai.com>
2023-05-18 18:18:15 +0700

    feat: :sparkles: add url validator function

--- projects/ngx-validator/src/lib/validators/validators.ts
+++ projects/ngx-validator/src/lib/validators/validators.ts
@@ -5,6 +5,8 @@ import {
   ValidationErrors,
 } from '@angular/forms';
 
+const URL_REGEXP = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
 function isObject(arg: any): boolean {
   return arg !== null && typeof arg === 'object' && !Array.isArray(arg);
 }
@@ -336,5 +338,15 @@ export function range(range: [number, number], message: string): ValidatorFn {
     return value < min || value > max
       ? { range: { range, message, actual: control.value } }
       : null;
+  };
+}
+
+export function url(message: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (isEmptyInputValue(control.value) || isEmptyInputValue(range)) {
+      return null; // don't validate empty values to allow optional controls
+    }
+
+    return URL_REGEXP.test(control.value) ? null : { url: { message } };
   };
 }
